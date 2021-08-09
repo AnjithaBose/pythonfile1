2b450d830245f9485d3668342c15b21953db065a
anjitha <anjitha1998pvr@gmail.com>
2021-08-09 15:58:12 +0530

    initial file is committed

---
+++ activity.py
@@ -0,0 +1,152 @@
+#create first list named as list1
+print("....list1 is created....")
+list1=["True","2+4j",'Anjitha',10]
+print(list1)
+#print type of the list
+print("First list types",type(list1))
+#copy list-copy the same list copy()
+print("......1:copylist")
+new_list11=list1.copy()
+print(new_list11,"The list is copied")
+#Add new item using append()
+print("......2:Add a new item")
+list1.append(14.56)
+print(list1,"Add a new item")
+#Access item of the list index()
+print("......3:Access item")
+print(list1.index(10),"index of 10:")
+#Remove item from list remove()
+print("......4:Remove the item")
+list1.remove('2+4j')
+print(list1,"successfully removed")
+#Replace a item from the list
+print("......5:Replace the item")
+list1[2]="merin"
+print(list1,"Replaced the item from the list")
+#join two list using plus operator
+list1=["True","2+4j",'Anjitha',10]
+list12=["rose","lotus","lilly"]
+list121=list1+list12
+print(list121,"Join two list")
+#create second list named as list2
+print("...list2 is created....")
+list2=["True",12,"lakshmi",'2i+5j']
+print(list2,"list2 is printed")
+#type of the list
+print("type of list is",type(list2))
+#copy the list with same list copy()
+print(".......1.copy the list")
+new_list22=list2.copy()
+print("copy the list",new_list22)
+#Add a new item to the list
+print(".....2.Access the item position")
+list2.append("kavya")
+print("A new item added",list2)
+#Access the index position
+print("......3.Add the item")
+list2.index("kavya")
+print("Position of the item",list2)
+print ("......4.Remove the item")
+list2.remove(12)
+print("Removed item is:",list2)
+print(".......5.Replace the file")
+list2[1]="Appu"
+print("item is replace",list2)
+print(".......6.join the list")
+lista=["sum","sub","div","mul"]
+listb=["maths","eng","phy"]
+listc=lista+listb
+print("The list is join",listc)
+#create Third list named as list3
+print("....list3 is created....")
+list3=["False","2i+9j",'keerthi',100]
+print(list3)
+#print type of the list
+print("First list types",type(list3))
+#copy list-copy the same list copy()
+print("......1:copylist")
+new_list33=list3.copy()
+print(new_list33,"The list is copied")
+#Add new item using append()
+print("......2:Add a new item")
+list3.append("true")
+print(list3,"Add a new item")
+#Access item of the list index()
+print("......3:Access item")
+print(list3.index("keerthi"),"index of keerthi:")
+#Remove item from list remove()
+print("......4:Remove the item")
+list3.remove('2i+9j')
+print(list3,"successfully removed")
+#Replace a item from the list
+print("......5:Replace the item")
+list3[3]="safe"
+print(list3,"Replaced the item from the list")
+#join two list using plus operator
+print("......6.Join the lists")
+listd=["pen","pencil","book"]
+liste=[1,2,3,4,]
+listf=listd+liste
+print("Join the list is",listf)
+#create forth list named as list4
+print("....list4 is created....")
+list4=["false","2i+6j",'rose',30]
+print(list4)
+#print type of the list
+print("First list types",type(list4))
+#copy list-copy the same list copy()
+print("......1:copylist")
+new_list44=list4.copy()
+print(new_list44,"The list is copied")
+#Add new item using append()
+print("......2:Add a new item")
+list4.append(100)
+print(list4,"Add a new item")
+#Access item of the list index()
+print("......3:Access item")
+print(list4.index("rose"),"index of 10:")
+#Remove item from list remove()
+print("......4:Remove the item")
+list4.remove('false')
+print(list4,"successfully removed")
+#Replace a item from the list
+print("......5:Replace the item")
+list4[1]="merin"
+print(list4,"Replaced the item from the list")
+#join two list using plus operator
+print("......6:Join the lists")
+listg=["abc","anu",11,"sum"]
+listh=[7,8,"sub",0]
+listi=listg+listh
+print(listi,"join two list is created")
+#create forth list named as list5
+print("....list5 is created....")
+list5=["false","4i+6j",'india',7]
+print(list5)
+#print type of the list
+print("First list types",type(list5))
+#copy list-copy the same list copy()
+print("......1:copylist")
+new_list55=list5.copy()
+print(new_list55,"The list is copied")
+#Add new item using append()
+print("......2:Add a new item")
+list5.append("kerala")
+print(list5,"Add a new item")
+#Access item of the list index()
+print("......3:Access item")
+print(list5.index("false"),"index of false:")
+#Remove item from list remove()
+print("......4:Remove the item")
+list5.remove('false')
+print(list5,"successfully removed")
+#Replace a item from the list
+print("......5:Replace the item")
+list5[3]="krishna"
+print(list5,"Replaced the item from the list")
+#join two list using plus operator
+print("......6:Join the lists")
+listj=["star","venus",11,"sun"]
+listk=[19.10,8,"earth","false"]
+listl=listj+listk
+print(listl,"join two list is created")
